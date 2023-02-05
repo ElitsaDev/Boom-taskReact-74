2d9a780f82c5030810d4ae7d7fbd5638edd3f7a2
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-05 15:19:18 +0200

    move all logic in App file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import "./App.css";
-import NumberValidator from "./NumberValidator";
+import { useState, useMemo } from "react";
+
+const checkInputIfIsNumber = (text) => {
+  let onlyDigits = new RegExp(/^[0-9]*$/gm);
+  return onlyDigits.test(text);
+}
 
 function App() {
+  const [text, setText] = useState("");
+
+  const isValidNumber = useMemo(() => checkInputIfIsNumber(text), [text]);
 
   return (
     <div className="App">
      <div className="control has-icons-right">
-        <NumberValidator />
+     <input
+                className="input is-large"
+                type="text"
+                placeholder="Enter number..."
+                value={text}
+                onChange={(event) => {
+                    setText(event.target.value);
+                }}
+            />
+            <span className="icon is-small is-right">
+                <i className={`fas ${isValidNumber ? "fa-check" : "fa-times"}`} />
+            </span>
       </div>
     </div>
   );
