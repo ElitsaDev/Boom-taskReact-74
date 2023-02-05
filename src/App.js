9cabdc44c84663d3accf6784901a1c8ff151b58f
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-05 13:13:43 +0200

    useState and useMemo to check input

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
+import NumberValidator from "./NumberValidator";
 
 function App() {
+
   return (
     <div className="App">
      <div className="control has-icons-right">
-        <input
-          className="input is-large"
-          type="text"
-          placeholder="Enter number..."
-
-        />
-        <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+        <NumberValidator />
       </div>
     </div>
   );
